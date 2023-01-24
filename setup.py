7da37a7053c97bcdf3e50d25955d25b843776870
lipicoder <lipicoder@qq.com>
2023-01-24 11:31:23 +0800

    add version

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 """Setup configuration for the package."""
 # -*- coding: utf-8 -*-
-try:
-    from setuptools import find_packages, setup
-except ImportError:
-    import distribute_setup
-
-    distribute_setup.use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+import versioneer
 
 # pylint: disable=invalid-name
 long_desc = """delibird is a python tool library based on Python pyarrow \
@@ -14,13 +9,11 @@ which supports multithread and asynchronous calls. \
 It can help users transform data 
 between database and Parquet files."""
 
-requires = ["pyarrow", "psycopy", "click"]
-
 setup(
     name="delibird",
-    version="0.0.6",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     url="https://github.com/lipicoder/delibird",
-    license="MIT License",
     author="lipi,zhujw",
     author_email="lipicoder@qq.com, kratoswittgenstein@gmail.com",
     description="Exchange data between database and parquet files",
@@ -33,5 +26,4 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requires,
 )
--- src/delibird/__init__.py
+++ src/delibird/__init__.py
@@ -3,3 +3,6 @@
 from delibird import util
 
 __all__ = ["util"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
